4c1d4b7f604c7a7174211b6968ea3baa70903ad1
ibadwaseem <aptechcoaching2022@gmail.com>
2024-12-04 21:56:20 +0500

    footer change

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -9,7 +9,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return ( 
-    <div className="bg-blue-50">
+    <div className="bg-white">
         <footer className="text-gray-20 body-font">
     <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
       <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
@@ -42,7 +42,7 @@ const Footer = () => {
             viewBox="0 0 24 24"
           >
             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-          </svg> */}<RiFacebookFill className="text-2xl hover:text-[#4a6bff]" />
+          </svg> */}<RiFacebookFill className="text-2xl hover:text-[#ff4949]" />
         </Link>
        
        
